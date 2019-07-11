2b710ed5e2f783e8ecb2ca32c8bd330b7f5bae9c
Dacosta <dacostadesarrollo@gmail.com>
2019-07-11 11:08:37 -0500

    nueva conexión

--- wp-config.php
+++ wp-config.php
@@ -26,10 +26,10 @@ define('DB_NAME', 'wordpress_db');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '12345');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '67.207.90.189');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
